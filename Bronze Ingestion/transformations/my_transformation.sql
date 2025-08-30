ad0d07a0eea1e86915efb8404a73c7bcf99c5bf4
Mahesh-Sarkar06 <Mahesh-Sarkar06>
2025-08-30 17:04:50 +0000

    Bronze layer ingestion

---
+++ Bronze Ingestion/transformations/my_transformation.sql
@@ -0,0 +1,108 @@
+-- Ingesting streaming album data
+CREATE OR REPLACE STREAMING TABLE bronze.album
+COMMENT 'Raw album data from volumes to bronze layer'
+AS
+SELECT * FROM cloud_files(
+  '/Volumes/apple/raw/album_files',
+  'csv',
+  map('header', 'true')
+);
+
+
+-- Ingesting streaming artists data
+CREATE OR REPLACE STREAMING TABLE bronze.artist
+COMMENT 'Raw artist data from volumes to bronze layer'
+AS
+SELECT * FROM cloud_files(
+  '/Volumes/apple/raw/artists_files',
+  'csv',
+  map('header', 'true')
+);
+
+
+-- Ingesting streaming customers data
+CREATE OR REPLACE STREAMING TABLE bronze.customer
+COMMENT 'Raw customer data from volumes to bronze layer'
+AS
+SELECT * FROM cloud_files(
+  '/Volumes/apple/raw/customers_files',
+  'csv',
+  map('header', 'true')
+);
+
+
+-- Ingesting streaming employee data
+CREATE OR REPLACE STREAMING TABLE bronze.employee
+COMMENT 'Raw employee data from volumes to bronze layer'
+AS
+SELECT * FROM cloud_files(
+  '/Volumes/apple/raw/employee_files',
+  'csv',
+  map('header', 'true')
+);
+
+
+-- Ingesting streaming genre data
+CREATE OR REPLACE STREAMING TABLE bronze.genre
+COMMENT 'Raw genre data from volumes to bronze layer'
+AS
+SELECT * FROM cloud_files(
+  '/Volumes/apple/raw/genre_files',
+  'csv',
+  map('header', 'true')
+);
+
+
+-- Ingesting streaming invoice inline data
+CREATE OR REPLACE STREAMING TABLE bronze.invoice_inline
+COMMENT 'Raw invoice inline data from volumes to bronze layer'
+AS
+SELECT * FROM cloud_files(
+  '/Volumes/apple/raw/invoice_inline_files',
+  'csv',
+  map('header', 'true')
+);
+
+
+-- Ingesting streaming invoice data
+CREATE OR REPLACE STREAMING TABLE bronze.invoice
+COMMENT 'Raw invoice data from volumes to bronze layer'
+AS
+SELECT * FROM cloud_files(
+  '/Volumes/apple/raw/invoices_files',
+  'csv',
+  map('header', 'true')
+);
+
+
+-- Ingesting streaming media type data
+CREATE OR REPLACE STREAMING TABLE bronze.mediatype
+COMMENT 'Raw media type data from volumes to bronze layer'
+AS
+SELECT * FROM cloud_files(
+  '/Volumes/apple/raw/media_type_files',
+  'csv',
+  map('header', 'true')
+);
+
+
+-- Ingesting streaming playlist data
+CREATE OR REPLACE STREAMING TABLE bronze.playlist
+COMMENT 'Raw playlist data from volumes to bronze layer'
+AS
+SELECT * FROM cloud_files(
+  '/Volumes/apple/raw/playlist_files',
+  'csv',
+  map('header', 'true')
+);
+
+
+-- Ingesting streaming track data
+CREATE OR REPLACE STREAMING TABLE bronze.track
+COMMENT 'Raw playlist_track data from volumes to bronze layer'
+AS
+SELECT * FROM cloud_files(
+  '/Volumes/apple/raw/track_files',
+  'csv',
+  map('header', 'true')
+);
